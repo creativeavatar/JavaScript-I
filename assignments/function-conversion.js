29b063943f9f13d7553cbf1b2c19f02e3fbed0d1
John Moura <john@buildreach.com>
2019-04-01 18:40:49 -0400

    Javascript Lesson 1 Complete with Stretch

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -4,11 +4,21 @@
 // console.log("Function was invoked!");
 // };
 // myFunction();
+let myFunction = () => {
+    console.log("Function was invoked!");
+};
+myFunction();
 
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
+
+let anotherFunction = (param) => {
+    return param;
+};
+anotherFunction("Example");
+console.log(anotherFunction("Example"))
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
@@ -21,6 +31,14 @@
 // subtract(1,2);
 
 
+let add = (param1, param2) => { return param1 + param2 };
+ add(1 + 2);
+console.log(add(1, 2))
+
+let subtract = (param1, param2) => { return param1 - param2 };
+subtract(1 - 2);
+console.log(subtract(1, 2))
+
 // Stretch
 
 // exampleArray = [1,2,3,4];
@@ -28,3 +46,8 @@
 //   return num * 3;
 // });
 // console.log(triple);
+exampleArray = [1, 2, 3, 4];
+const triple = exampleArray.map((num) => {
+    return num * 3;
+});
+console.log(triple);
